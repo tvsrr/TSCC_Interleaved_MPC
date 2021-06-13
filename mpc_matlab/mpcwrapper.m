eac50fa80b1758bf82f5b6b6e8d685ed473c975c
Raghu Ram <32590292+tvsrr@users.noreply.github.com>
2021-06-13 15:45:18 +0530

    Made MPC wrapper runnable

--- mpc_matlab/mpcwrapper.m
+++ mpc_matlab/mpcwrapper.m
@@ -1,2 +1,24 @@
-%mpc2torcs 
-[vsend,wsend,xf,yf,thf]=launch(68.95,11.37,0,0,0,50,0);
+% File to Run MPC Matlab
+% Initial Point of the Ego Vehicle
+x0 = 0;
+y0 = 0;
+
+% Obstacle position
+x_ob = 25;
+y_ob = 0;
+
+% Initial Theta of the ego vehicle
+theta_in = 0;
+
+% Other flags and parameters
+ctrl_horizon = 10;
+stop_obstacle_iteration = 1;
+enable_obstacle_avoidance = 1;
+
+% Main Function that runs MPC
+[stateRobo,stateObst,vlast,wlast,...
+    vsend,wsend,v_movx,v_movy]        =   launch_iros(x0, y0, x_ob,...
+                                                      y_ob, theta_in,...
+                                                      ctrl_horizon, stop_obstacle_iteration,...
+                                                      enable_obstacle_avoidance);
+                                                                    
