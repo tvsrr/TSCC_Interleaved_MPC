e78910343fca04c77bc599c781a6cf31d985874d
Raghu Ram <32590292+tvsrr@users.noreply.github.com>
2023-05-18 12:06:03 +0530

    Update launch _ iros_usage.m
    Rewritten code with better variable names

--- mpc_matlab/launch _ iros_usage.m
+++ mpc_matlab/launch _ iros_usage.m
@@ -1,44 +1,56 @@
-%launch file 
-%contains all the necessary function- calls
-%The dependant functions are linerisation of the values,finding A,Q,C
-% Formulatng the cost functons 
-%This document will have the cvx- optimization
-%Rectangles will be a function
-%Obstacle avoidance for 3 circles will be afunction 
-%Plotting will be another function
+% Clear workspace and command window
 clear all;
 clc;
- [x0,y0,xg,yg,w_guess,v_guess,theta_in,theta_f,n,delt,x_points_left,y_points_left,x_points_right,y_points_right] = get_input();
- ct=20;
-while(ct)
- [coeffs_fin_x,coeffs_vel_x,coeffs_fin_y,coeffs_vel_y,x_cod,y_cod,const_x,const_y,w_x]= linearise_hol(n,x0,y0,w_guess,v_guess ,delt,theta_in);
- num = 40;
- [A,Q,C]= mat_creator(coeffs_fin_x,coeffs_vel_x,coeffs_fin_y,coeffs_vel_y,const_x,const_y,num,xg,yg);
 
+% Get input parameters
+[start_x, start_y, goal_x, goal_y, angular_rate_guess, velocity_guess, start_theta, end_theta, num_points, time_step, left_x_points, left_y_points, right_x_points, right_y_points] = get_input();
 
+% Initialize optimization iteration counter
+iteration_counter = 20;
+
+% Start optimization iteration
+while(iteration_counter)
+    % Linearize the non-holonomic system
+    [x_coeffs_final, x_coeffs_velocity, y_coeffs_final, y_coeffs_velocity, x_const, y_const, angular_rate_x]= linearise_hol(num_points, start_x, start_y, angular_rate_guess, velocity_guess, time_step, start_theta);
+
+    % Create matrices for optimization
+    [A_matrix, Q_vector, C_scalar] = mat_creator(x_coeffs_final, x_coeffs_velocity, y_coeffs_final, y_coeffs_velocity, x_const, y_const, num_points, goal_x, goal_y);
  
- %% optimization
-cvx_begin quiet
-variables w_cvx(1,n) vel(1,n)
-new= [w_cvx,vel];
-fin3=theta_in+sum(w_cvx)*delt;
-fin = new*A*new'+Q*new'+C+(fin3-theta_f)^2+ 10*(w_cvx(1)-0)^2 + 10*(w_cvx(end)-0)^2+(10*(vel(1)-0)^2 + 10*(vel(end)-0)^2);
-minimize(fin) 
+    %% Optimization using CVX
+    cvx_begin quiet
+        % Define optimization variables
+        variables angular_rate_cvx(1, num_points) velocity(1, num_points)
+        new= [angular_rate_cvx, velocity];
+        
+        % Define cost function
+        final_theta = start_theta + sum(angular_rate_cvx) * time_step;
+        cost_function = new * A_matrix * new' + Q_vector * new' + C_scalar + (final_theta - end_theta)^2 + 10 * (angular_rate_cvx(1) - 0)^2 + 10 * (angular_rate_cvx(end) - 0)^2 + (10 * (velocity(1) - 0)^2 + 10 * (velocity(end) - 0)^2);
+        
+        % Minimize cost function
+        minimize(cost_function) 
 
-subject to 
--0.5<=w_cvx<=0.5;
-0<=vel<=5;
-v_guess - 0.5 <= vel <= v_guess + 0.5;
-w_guess-0.5<=w_cvx<=w_guess+0.5;
--2.2*delt <= w_cvx(2:n) - w_cvx(1:(n-1))<= 2.2*delt;
--2.2*delt <= vel(2:n) - vel(1:(n-1))<= 2.2*delt;
--0.05<= w_cvx(1)<=0.05;
--0.05<= w_cvx(n)<=0.05;
-cvx_end
-v_guess = vel;
-w_guess = w_cvx;
-cvx_optval
-ct = ct-1
-[x1,y1]=plot_karo(x0,y0,xg,yg,v_guess,w_guess,theta_in,delt,n);
+        % Define constraints
+        subject to 
+            -0.5 <= angular_rate_cvx <= 0.5;
+            0 <= velocity <= 5;
+            velocity_guess - 0.5 <= velocity <= velocity_guess + 0.5;
+            angular_rate_guess - 0.5 <= angular_rate_cvx <= angular_rate_guess + 0.5;
+            -2.2 * time_step <= angular_rate_cvx(2:num_points) - angular_rate_cvx(1:(num_points-1)) <= 2.2 * time_step;
+            -2.2 * time_step <= velocity(2:num_points) - velocity(1:(num_points-1)) <= 2.2 * time_step;
+            -0.05 <= angular_rate_cvx(1) <= 0.05;
+            -0.05 <= angular_rate_cvx(num_points) <= 0.05;
+    cvx_end
+    
+    % Update angular rate and velocity guess with optimization results
+    velocity_guess = velocity;
+    angular_rate_guess = angular_rate_cvx;
+    
+    % Output optimization value
+    cvx_optval
+    
+    % Decrement iteration counter
+    iteration_counter = iteration_counter - 1;
+
+    % Plot path after each optimization iteration
+    [x_path, y_path] = plot_karo(start_x, start_y, goal_x, goal_y, velocity_guess, angular_rate_guess, start_theta, time_step, num_points);
 end
-
